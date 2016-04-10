cff2cda701eccfd937d1896464dc7eeb3874671e
ppypenko <ppypenko@student.neumont.edu>
2016-04-10 17:28:08 -0600

    Some file missing.

---
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return context.Products;
+            }
+        }
+    }
+}
