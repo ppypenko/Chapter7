b86d6409e9444991c862e9dd326c682d12896ff9
ppypenko <ppypenko@student.neumont.edu>
2016-04-10 13:58:45 -0600

    First step complete.

---
+++ SportsStore/SportsStore.WebUI/Infastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
